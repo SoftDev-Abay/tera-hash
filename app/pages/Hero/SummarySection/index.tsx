059152209f44affb35a97a5feba78558e3486bc9
SoftDev-Abay <abay.aliev.03@gmail.com>
2024-08-02 17:05:52 +0500

    created structure for summary section

---
+++ app/pages/Hero/SummarySection/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Roadmap from "./Roadmap/Roadmap";
+import Tips from "./Tips/Tips";
+import CallToAction from "./CallToAction/CallToAction";
+
+const SummarySection = () => {
+  return (
+    <div className="padding-wrapper container-max-width-1920 summary-section">
+      <Tips />
+      <Roadmap />
+      <CallToAction />
+    </div>
+  );
+};
+
+export default SummarySection;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,15 @@ import Wrapper from "@/app/pages/Wrapper/Wrapper";
 import TitleSection from "@/app/pages/Hero/TitleSection/TitleSection";
 import StorySection from "@/app/pages/Hero/StorySection/StorySection";
 import AboutSection from "@/app/pages/Hero/AboutSection";
-
+import SummarySection from "@/app/pages/Hero/SummarySection";
 const About = () => {
   return (
     <>
       <Wrapper>
         <TitleSection />
-        <StorySection/>
+        <StorySection />
         <AboutSection />
+        <SummarySection />
       </Wrapper>
     </>
   );
