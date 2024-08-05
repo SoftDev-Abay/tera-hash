a7ce79503587d0d124bca7b67f473549d2e89207
ayanhan <kalkhozhaev.j@gmail.com>
2024-08-05 18:58:03 +0900

    fix: added correct domain

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['your-domain.com'], // Replace with your actual domain
+    domains: ['https://tera-hash.vercel.app'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
-  compress: true, // Enable gzip compression
+  compress: true,
 };
 
 export default nextConfig;
